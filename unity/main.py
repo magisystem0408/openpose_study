c439feb34f69333dd1555444a32342391733cacf
magisystem0408 <masato.matsudo@gmail.com>
2021-05-26 00:31:15 +0900

    add project

---
+++ unity/main.py
@@ -0,0 +1,5 @@
+import unity.controllers.server
+
+if __name__ == '__main__':
+    unity.controllers.server.run()
+
---
+++ unity/models/manager.py
@@ -0,0 +1,11 @@
+import sys
+import time
+import subprocess
+
+import pyautogui
+
+class manager(object):
+
+    def pressKey(self):
+        time.sleep(5)
+        pyautogui.press('j')
